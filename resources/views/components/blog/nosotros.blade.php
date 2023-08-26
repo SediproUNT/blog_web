45826aac25912e9481dd6f735a0efb36d67d3206
Carlos Alberto Pingo Aguilar <carlos.pingo@wedox.co>
2023-08-26 03:28:35 -0500

    Nosotros

--- resources/views/components/blog/nosotros.blade.php
+++ resources/views/components/blog/nosotros.blade.php
@@ -49,7 +49,7 @@
             <div class="row">
                 <div class="col-12 banner-box">
                     <header class="banner-heading content-news">
-                        <h1 style="">Nosotros</h1>
+                        <h1>Nosotros</h1>
                         <nav aria-label="breadcrumb" class="breadcrumb-header">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a style="text-decoration: none;"
@@ -62,4 +62,80 @@
             </div>
         </div>
     </div>
+
+    <section class="bg-light position-relative">
+        <div class="container py-5">
+
+            <div class="col-lg-12 m-auto text-center my-4">
+                <span class="subtitle">QUIENES SOMOS</span>
+                <h2 class="h1 title-section">Nosotros</h2>
+            </div>
+
+            <div class="row mb-5 nosotros">
+
+                <div class="col-lg-6 m-auto text-justify">
+                    <span class="text-nosotros">
+                        Somos la Sección Estudiantil de Dirección de Proyectos de la UNT, equipo multidisciplinario
+                        orientado a digundir las buenas prácticas en gestión de proyectos con métodología PMI y herramientas ágiles,
+                        bajo 3 enfoques académico,social y ambiental.
+                    </span>
+
+                    <div class="li mt-5">
+                        <div class="icon">
+                            <div><i class="fa fa-thumbtack" aria-hidden="true"></i></div>
+                        </div>
+                        <div>
+                            <h6>Nuestra Misión:</h6>
+                            <p class="text-nosotros">Formar a los futuros lideres en las buenas practicas de Dirección de Proyectos bajo un enfoque integral e innovador.</p>
+                        </div>
+                    </div>
+
+                    <div class="li">
+                        <div class="icon">
+                            <div><i class="fa fa-eye" aria-hidden="true"></i></div>
+                        </div>
+                        <div>
+                            <h6>Nuestra Visión:</h6>
+                            <p class="text-nosotros">Ser la mejor SEDIPRO a nivel nacional a partir de los mejores gestores de proyectos.</p>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="col-lg-6 m-auto">
+                    <img src="/assets/img/logo-sedipro.png" alt="logo_sedipro" class="img-fluid">
+                </div>
+
+            </div>
+
+
+
+
+            <div class="row mt-5 mb-5">
+
+                <div class="col-lg-6 m-auto mt-5 position-relative">
+                    <img src="/assets/img/nosotros.jpeg" style="padding: 0 2rem;" alt="logo_sedipro" class="img-fluid">
+                    <div class="experience">
+                        <span class="year">10 años</span>
+                        <span class="caption">proyectando vidas</span>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 m-auto text-justify mt-5">
+                    <span class="subtitle">HISTORIA</span>
+                    <h2 class="h1 title-section mb-3">Nuestra Historia</h2>
+
+                    <p class="text-nosotros">En el año 2013, nació Sedipro UNT con un propósito claro: difundir las buenas prácticas de dirección de proyectos bajo un enfoque integral.</p>
+                    <p class="text-nosotros">Hoy, al celebrar nuestros 10 años, miramos hacia el futuro con emoción y determinación.</p>
+                    <p class="text-nosotros">Continuaremos impulsando la excelencia y promoviendo la ética en la dirección de proyectos, buscando siempre la innovación y la mejora continua.</p>
+                    <p class="text-nosotros">¡Felices 10 años, Sedipro UNT! Sigamos dejando huellas en la historia y proyectando vidas hacia un futuro brillante.</p>
+                </div>
+    
+            </div>
+
+
+
+
+        </div>
+    </section>
+
 @endsection
